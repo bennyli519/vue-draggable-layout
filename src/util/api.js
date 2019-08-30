6244a62cd45927ea1ccf06aa5fc844d9587632b9
Benny <benny_lzb@meten.com>
2019-08-30 10:14:55 +0800

    .

--- src/util/api.js
+++ src/util/api.js
@@ -2,13 +2,15 @@
  * @Description: 
  * @Author: Benny
  * @Date: 2019-08-26 11:20:08
- * @LastEditTime: 2019-08-26 17:57:35
+ * @LastEditTime: 2019-08-30 10:14:49
  */
 const devSiteUrl = {
     SITE: "/api",
 }
 const proSiteUrl = {
-    SITE: "https://app.workiee.com/ptExt/fzy/diy/webApi"
+    // SITE: "https://app.workiee.com/ptExt/fzy/diy/webApi"
+    SITE: "../webApi"
+
 }
 
 const siteUrls = process.env.NODE_ENV !== 'production' ? devSiteUrl : proSiteUrl;
